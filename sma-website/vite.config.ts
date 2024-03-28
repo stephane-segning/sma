281ad8090bfe0a6d31071c52cd1763cadebb28b7
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2024-03-28 12:00:43 +0100

    feat: basic configuration for PWA

--- sma-website/vite.config.ts
+++ sma-website/vite.config.ts
@@ -1,6 +1,7 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 
 // https://vitejs.dev/config/
@@ -8,5 +9,11 @@ export default defineConfig({
     plugins: [
         tsconfigPaths(),
         react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            }
+        }),
     ],
 })
